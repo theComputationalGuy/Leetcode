80f02bd5920810386a053c517c1fd969d1ca3b27
theComputationalGuy <khalidshaikh848685@gmail.com>
2024-01-07 09:57:34 +0530

    solved 413. Arthmetic Slices

--- 413. Arithmetic Slices.cpp
+++ 413. Arithmetic Slices.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+
+        for(int i=2;i<nums.size();i++){
+            int n1 = nums[i-1];
+            int n2 = nums[i-2];
+            if(n1-n2 == nums[i]-n1){
+                dp[i] = dp[i-1] + 1;
+            }
+        }
+        int solution = 0;
+        for(int i=0;i<nums.size();i++){
+            solution += dp[i];
+        }
+
+        return solution;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums;
+    int input;
+    while(cin>>input){
+        nums.push_back(input);
+    }
+    cout<<s.numberOfArithmeticSlices(nums)<<endl;
+}
